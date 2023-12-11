eec09121b6ca8a4e40cbfbd33a9c1533fbd640e3
ely896 <saretta2011@libero.it>
2023-12-11 18:36:18 +0100

    Add Milestone 0

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
+/*
+Utilizzando i dati forniti, creare un array di stringhe con i nomi dei membri del team.
+MILESTONE 0:
+Creare l’array di stringhe contenente i nomi dei membri del team.
+MILESTONE 1:
+Stampare le informazioni su DOM come card.
+MILESTONE 3:
+Aggiungere un evento click sulla card che aggiunge/rimuove una classe per evidenziare un componente del team.
+BONUS 1:
+In generale curare la parte di UI e organizzare i singoli membri in card/schede.
+BONUS 2:
+Inserire un form per l’aggiunta di un elemento alla lista.
+*/
+
 console.log('funziona');
+
+//crea array di stringhe con i nome dei membri del team
+
+const membersTeam = ['Wayne Barnett', 'Angela Caroll', 'Walter Gordon', 'Angela Lopez', 'Scott Estrada', 'Barbara Ramos'];
+
+console.log(membersTeam);
+
+//MILESTONE 1:
+//Stampare le informazioni su DOM come card.
+
+`<div class="card">
+<div class="card-body">
+    nome membro del team
+</div>`
+
+
+
+  
